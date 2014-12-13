0cf02b6991f8a0e9687279386bd845d813701246
Igor <yarimmer@gmail.com>
2014-12-13 06:08:56 +0200

    Initial commit

---
+++ app/src/variantTwo/java/solutions/rii/kirovogradmobilemeetup2014/ValidateEmail.java
@@ -0,0 +1,12 @@
+package solutions.rii.kirovogradmobilemeetup2014;
+
+/**
+ * KirovogradMobileMeetup2014
+ * <p/>
+ * Created by Igor on 13.12.2014.
+ */
+public class ValidateEmail {
+    public static boolean validate(String email) {
+        return email.contains("foo");
+    }
+}
